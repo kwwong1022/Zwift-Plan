8aab96aeb0bb19728be31133251020eaeadd5fc5
xKiraEzy <69613966+xKiraEzy@users.noreply.github.com>
2021-07-06 22:58:03 +0800

    Push

--- js/app.js
+++ js/app.js
@@ -24,3 +24,34 @@ visualViewport.addEventListener('resize', function() {
         }
     });
   });
+
+var textGlob = "Starter \n";
+var Form,VarA , VarB;
+function createText(filename)
+{
+    var text = textGlob;
+    Form = document.getElementById("formMain");
+    VarA = Form.elements['itemA'];
+    VarB = Form.elements['itemB'];
+    var msg = [];
+    msg[0] = VarA.value;
+    msg[1] = VarB.value;
+    for (let index = 0; index < msg.length; index++) {
+        text+=msg[index];
+        text += " & ";
+    }
+    download(filename, text)
+}
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+}
